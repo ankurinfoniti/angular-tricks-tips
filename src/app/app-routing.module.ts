c5a7541cf67db1db3ab6e7564be0ee94d0952a9d
Ankur Mishra <ankur.mishra@orange.com>
2022-12-14 12:35:32 +0530

    html to pdf download

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HtmlToPdfComponent } from './html-to-pdf/html-to-pdf.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'html-to-pdf',
+    component: HtmlToPdfComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { KeyValue } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-fifteen-practice';
+export class AppComponent implements OnInit {
+  items: KeyValue<string, string>[] = [];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        key: 'HTML TO PDF',
+        value: '/html-to-pdf',
+      },
+      {
+        key: 'HTTP Caching',
+        value: '/http-caching',
+      },
+    ];
+  }
 }

bd549b5e4183b63af7b53b3a10119ff19a831b52
ankurinfoniti <ankur.infoniti@gmail.com>
2023-05-12 23:22:02 +0530

    creating new task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +19,7 @@ import { CacheInterceptor } from './caching/core/cache.interceptor';
 import { ImageSliderComponent } from './image-slider/image-slider.component';
 import { TaskComponent } from './task/task.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +29,7 @@ import { TaskDetailComponent } from './task-detail/task-detail.component';
     ImageSliderComponent,
     TaskComponent,
     TaskDetailComponent,
+    TaskDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,6 +39,10 @@ import { TaskDetailComponent } from './task-detail/task-detail.component';
     BrowserAnimationsModule,
     MatCardModule,
     DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [
     {
